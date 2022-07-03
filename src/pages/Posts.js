a384abd7aa3e6950e7bdf6c6be33c61549b58fb6
Oleksii Kuryliuk <oleksii.kuryliuk@O-KURYLIUK.local>
2022-07-04 00:06:36 +0300

    made input controled

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import Pagination from '../components/Pagination'
-const sourceUrl = "https://jsonplaceholder.typicode.com/posts"
+import React, { useEffect, useState } from "react";
+import Pagination from "../components/Pagination";
+const sourceUrl = "https://jsonplaceholder.typicode.com/posts";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [numberPages, setNumberPages] = useState(0);
   const [activePost, setActivePost] = useState(1);
   const [postsFoDisplay, setPostsFoDisplay] = useState([]);
-  const [postsPerPage, setPostsPerPage] = useState(5)
+  const [postsPerPage, setPostsPerPage] = useState(5);
+  const [inputValue, setInputValue] = useState(5);
 
   function handlerInput(e) {
-    let newPostsPerPage = e.target.value
-    if (newPostsPerPage <= 0) {
-      setPostsPerPage(0)
-      setNumberPages(1)
-      return
-    }
-    if (newPostsPerPage > posts.length) {
-      setPostsPerPage(posts.length)
-      setNumberPages(1)
-      return
-    }
-    setPostsPerPage(newPostsPerPage)
-    setNumberPages(Math.ceil(posts.length / newPostsPerPage))
+    setActivePost(1);
+    setInputValue(e.target.value);
   }
+
+  useEffect(() => {
+    if (posts.length === 0) {
+      return;
+    }
+
+    if (inputValue <= 0) {
+      setPostsPerPage(0);
+      setNumberPages(1);
+      return;
+    }
+    if (inputValue > posts.length) {
+      setPostsPerPage(posts.length);
+      setNumberPages(1);
+      return;
+    }
+    setPostsPerPage(inputValue);
+    setNumberPages(Math.ceil(posts.length / inputValue));
+  }, [inputValue]);
 
   useEffect(() => {
     fetch(sourceUrl)
@@ -36,31 +45,41 @@ const Posts = () => {
 
   useEffect(() => {
     if (posts.length === 0) {
-      return
+      return;
     }
-    const postsPart = posts.slice((activePost - 1) * postsPerPage, activePost * postsPerPage);
-    setPostsFoDisplay(postsPart)
+    const postsPart = posts.slice(
+      (activePost - 1) * postsPerPage,
+      activePost * postsPerPage
+    );
+    setPostsFoDisplay(postsPart);
   }, [posts, activePost, postsPerPage]);
 
   return (
     <>
       <input
-        type="number"
-        className='setPostsQuantity'
+        value={inputValue}
         onChange={handlerInput}
+        type="number"
+        className="setPostsQuantity"
       ></input>
 
-      <span style={{ fontSize: "18px", fontWeight: 600 }}>Set posts per page</span>
+      <span style={{ fontSize: "18px", fontWeight: 600 }}>
+        Set posts per page
+      </span>
 
       {postsFoDisplay.map((item) => {
         return (
           <div key={item.id}>
             <div>id: {item.id}</div>
-            <div>Post Title: <b>{item.title}</b></div>
-            <div>Post body: <i>{item.body}</i></div>
+            <div>
+              Post Title: <b>{item.title}</b>
+            </div>
+            <div>
+              Post body: <i>{item.body}</i>
+            </div>
             <hr></hr>
           </div>
-        )
+        );
       })}
       <Pagination
         numberPages={numberPages}
@@ -68,7 +87,7 @@ const Posts = () => {
         setActivePost={setActivePost}
       ></Pagination>
     </>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
